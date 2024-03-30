a0bf5347e51b0e311ceec0113a60308aaaecce72
mdriscol <122190993+mdriscol@users.noreply.github.com>
2024-03-29 22:01:24 -0400

    Update server.js
    updated to gpt-3.5-turbo-instruct

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.post("/", async (req, res) => {
     const prompt = req.body.prompt;
 
     const response = await openai.createCompletion({
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-instruct",
       prompt: `${prompt}`,
       temperature: 0, // Higher values means the model will take more risks.
       max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
